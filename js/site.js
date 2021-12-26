740f3d8a7b291de7f44b140f236c06ab4567a967
Kevin MacKenzie <kevin@homesmoncton.com>
2021-12-26 15:18:15 -0400

    Completed the javascript and updated the code display page

--- js/site.js
+++ js/site.js
@@ -1,5 +1,47 @@
-// call helloWorld
+function getString(){
+    // make the alert invisible
+    document.getElementById("alert").classList.add("invisible");
 
-function helloWorld() {
-    alert(`Hello World`);
+    // get the input from the form
+    let initValue = document.getElementById("theString").value;
+
+    let returnObj = checkForPalindrone(initValue);
+    
+    displayMessage(returnObj);
+}
+
+// check if a palindrone
+function checkForPalindrone(initValue) {
+
+    // convert to lowercase
+    initValue = initValue.toLowerCase();
+    //remove any spaces and other special characters
+    let regex = /[^a-z0-9]/gi;
+    initValue = initValue.replace(regex, "");
+
+    let revString = [];
+    let returnObj = {};
+
+    for (let index = initValue.length - 1; index >= 0; index--) {
+        revString += initValue[index];
+    }
+
+    if (revString == initValue){
+        returnObj.msg = "Well Done! You've entered a Palindrone.";
+    }
+    else {
+        returnObj.msg = "Sorry, you did not enter a Palindrone.";
+    }
+
+    returnObj.reversed = revString;
+    return returnObj;
+}
+
+
+//display the message to screen
+function displayMessage(returnObj) {
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is ${returnObj.reversed}`;
+    // make the alert visible
+    document.getElementById("alert").classList.remove("invisible");
 }
